d1badf8d621e358a5aa9c1973dfd333d87effc3d
Jhundon Mendi <84818535+JMendi2021@users.noreply.github.com>
2022-05-22 05:28:36 +0100

    Update Linear Regression Gradient Descent.py

--- Linear Regression Gradient Descent.py
+++ Linear Regression Gradient Descent.py
@@ -1,27 +1,32 @@
+#Change as required
+data = [[1,1],[2,5], [3,11]]
 a = 0.1
+
+#Gradient Descent
 w_0 = 0
 w_1= 0
-
-data = [[1,1],[2,5], [3,11]]
-
 print("")
 for i in range(len(data)):
-    x = data[i][0]
+    #Input Attributes
+    x_1 = data[i][0]
+
+    #Desired Output
     y = data[i][1]
     
     #Gradient Descent Step
     f = (w_1 * x) + w_0
     w_0 = w_0 - a * (f - y)
-    w_1 = w_1 - a * (f - y) * x
+
+    #Put other w's in this section below if Number of Attributes > 1 (Remember Partial Derviatives)
+    w_1 = w_1 - a * (f - y) * x_1
     
-    #Mean Square Cost of Current Iteration
+    #Mean Square Cost of Current Model
     cost = 0
     for j in range(len(data)):
-        x_2 = data[j][0]
-        y_2 = data[j][1]
-        f_2 = (w_1 * x_2) + w_0
-        cost = cost + (f_2-y_2)**2
-        
+        x_j = data[j][0]
+        y_j = data[j][1]
+        f_j = (w_1 * x_j) + w_0
+        cost = cost + (f_j-y_j)**2
     mean_cost = cost/len(data)
 
     print("Iteration", (i+1))
